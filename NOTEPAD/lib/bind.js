61c6294fca8ab1bdea826e1826efb8ea1522f191
limitwh <limitwh@163.com>
2018-03-23 00:49:32 +0800

    bind test

--- NOTEPAD/lib/bind.js
+++ NOTEPAD/lib/bind.js
@@ -1,7 +1,23 @@
-h5.core.view.bind('#todolist',{
-     title: 'data bind test',
-     todoitem:{
-        text:"bind test",
-        time:"2018"
-     }
+var manager = h5.core.data.createManager('TestManager');
+
+var model = manager.createModel({
+    name: 'Bindtest',
+    schema: {
+        text: {
+            id: true
+        },
+        time: {
+            type: 'string'
+        }
+    }
 });
+
+var item = model.create({
+        text: 'Bind Test',
+        time: '2018'
+});
+
+h5.core.view.bind($('#todolist'),{
+     title: 'data bind test',
+     todoitem:item
+ });
