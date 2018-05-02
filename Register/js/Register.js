54470835148efaf38e9b07e964e6e53db7ecf21d
limitwh <limitwh@163.com>
2018-05-02 23:39:57 +0800

    添加了注册按钮，密码强度提示未添加

--- Register/js/Register.js
+++ Register/js/Register.js
@@ -7,69 +7,38 @@
                 validating: 'glyphicon glyphicon-refresh'
             },
             fields: {
-                loginame: {
-                    message: '用户名验证失败',
-                    validators: {
-                        notEmpty: {
-                            message: '用户名不能为空'
-                        },
-                        stringLength: {
-                            min: 6,
-                            max: 18,
-                            message: '用户名长度必须在6到18位之间'
-                        }
-                    }
-                },
-                /*nickname: {
-                    message: '昵称验证失败',
-                    validators: {
-                        notEmpty: {
-                            message: '昵称不能为空'
-                        },
-                        stringLength: {
-                            min: 6,
-                            max: 18,
-                            message: '用户名长度必须在6到18位之间'
-                        }
-                    }
-                }*/
-                loginemail: {
-                    message: '昵称验证失败',
-                    validators: {
-                        notEmpty: {
-                            message: '昵称不能为空'
-                        },
-                        emailAddress: {
-                            message: '邮箱地址格式有误'
-                        }
-                    }
-                }/*,
-                password1: {
+                Password1: {
                     validators: {
                         notEmpty: {
                             message: '请填写密码'
                         },
                         stringLength: {
-                            min: 6,
-                            message: '密码不少于6位'
+                            min: 8,
+                            mix: 20,
+                            message: '密长度码在8到12位之间'
+                        },
+                        identical: {
+                            field: 'Password2',
+                            message: '两次输入的密码不一致'
                         }
                     }
                 },
-                password2: {
+                Password2: {
                     validators: {
                         notEmpty: {
                             message: '请填写确认密码'
                         },
                         stringLength: {
-                            min: 6,
-                            message: '密码不少于6位'
+                            min: 8,
+                            mix: 20,
+                            message: '密长度码在8到12位之间'
                         },
                         identical: {
-                            field: 'password1',
-                            message: '确认密码与密码不一致'
+                            field: 'Password1',
+                            message: '两次输入的密码不一致'
                         }
                     }
-                }*/
-            }
+                }
+            },
     });
 });
